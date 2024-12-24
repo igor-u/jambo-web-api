71f533e0695d76f77c9bab510e882713c7dccc69
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-12-24 09:08:14 -0300

    feat: SolarPanelController w/ controller mapping

---
+++ Controllers/SolarPanelController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Jambo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Jambo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SolarPanelController : ControllerBase
+    {
+        private readonly ILogger<SolarPanelController> _logger;
+
+        public static List<SolarPanel> solarPanels = new List<SolarPanel>();
+        public SolarPanelController(ILogger<SolarPanelController> logger)
+        {
+            _logger = logger;
+        }
+
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View("Error!");
+        // }
+
+        [HttpPost]
+        public void AddSolarPanel([FromBody] SolarPanel solarPanel)
+        {
+            solarPanels.Add(solarPanel);
+        }
+    }
+}
