35f70575f177627c4013a29d25fb2559c958e590
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-12-24 09:22:48 -0300

    feat: SolarPanel validations

--- Models/SolarPanel.cs
+++ Models/SolarPanel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Jambo.Models
 {
     public class SolarPanel
     {
+        [Required]
+        [Length(12, 12)]
         public required string SerialNumber { get; set; }
+        [Required]
+        [Range(40, 600)]
         public int Power { get; set; }
     }
 }
